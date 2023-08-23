28ead426b3b0ebe76519c92228fe6bcceed559c8
AfandiyevMusa <113282393+AfandiyevMusa@users.noreply.github.com>
2023-08-23 14:18:54 -0400

    search is ready

---
+++ Final_Project/Controllers/CommonController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Final_Project.ViewModels;
+using Final_Project.Data;
+using Final_Project.Services.Interfaces;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Final_Project.Controllers
+{
+    public class CommonController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CommonController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult SearchMovie(string search)
+        {
+            SearchPartialVM searchPartialVM = new SearchPartialVM();
+            if (!String.IsNullOrEmpty(search))
+            {
+                searchPartialVM.Films = _context.Films.Where(m => m.Name.ToLower().Contains(search.ToLower())).Take(2).OrderByDescending(n => n.Id).ToList();
+            }
+
+            return PartialView("_SearchMoviePartialView", searchPartialVM);
+        }
+
+        public IActionResult SearchBlog(string search)
+        {
+            SearchPartialVM searchPartialVM = new SearchPartialVM();
+            if (!String.IsNullOrEmpty(search))
+            {
+                searchPartialVM.Blogs = _context.Blogs.Where(m => m.Title.ToLower().Contains(search.ToLower())).Take(2).OrderByDescending(n => n.Id).ToList();
+            }
+
+            return PartialView("_SearchBlogPartialView", searchPartialVM);
+        }
+    }
+}
+
---
+++ Final_Project/wwwroot/js/search.js
@@ -0,0 +1,26 @@
+﻿$(document).on('keyup', '#input-search', function () {
+    console.log($(this).val());
+    let inputVal = $(this).val().trim();
+    $("#searchList li").slice(0).remove();
+    $.ajax({
+        url: "/Common/SearchMovie?search=" + inputVal,
+        method: "get",
+        success: function (res) {
+            $("#searchList").append(res)
+        }
+    })
+});
+
+
+$(document).on('keyup', '#inputBlog-search', function () {
+    console.log($(this).val());
+    let inputVal = $(this).val().trim();
+    $("#searchBlogList li").slice(0).remove();
+    $.ajax({
+        url: "/Common/SearchBlog?search=" + inputVal,
+        method: "get",
+        success: function (res) {
+            $("#searchBlogList").append(res)
+        }
+    })
+});
